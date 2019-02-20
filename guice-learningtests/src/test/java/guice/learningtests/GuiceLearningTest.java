61953d5a30a08fdc764daca9f1262708580f34dc
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-02-20 16:06:54 +0100

    MyService and MyClient

--- guice-learningtests/src/test/java/guice/learningtests/GuiceLearningTest.java
+++ guice-learningtests/src/test/java/guice/learningtests/GuiceLearningTest.java
@@ -1,5 +1,19 @@
 package guice.learningtests;
 
+import com.google.inject.Inject;
+
 public class GuiceLearningTest {
 
+	private static class MyService {
+
+	}
+
+	private static class MyClient {
+		MyService service;
+
+		@Inject
+		public MyClient(MyService service) {
+			this.service = service;
+		}
+	}
 }
