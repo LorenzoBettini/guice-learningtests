9b6d8d83c7c4a4a052f7325f60b4f35f8bc40b86
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-02-20 16:57:14 +0100

    field and method injection

---
+++ guice-learningtests/src/test/java/guice/learningtests/GuiceFieldAndMethodInjectionLearningTest.java
@@ -0,0 +1,52 @@
+package guice.learningtests;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+import com.google.inject.Module;
+
+public class GuiceFieldAndMethodInjectionLearningTest {
+
+	private static interface IMyService {
+
+	}
+
+	private static class MyService implements IMyService {
+
+	}
+
+	private static class MyClientWithInjectedField {
+		@Inject
+		IMyService service;
+	}
+
+	private static class MyClientWithInjectedMethod {
+		IMyService service;
+
+		@Inject
+		public void init(IMyService service) {
+			this.service = service;
+		}
+	}
+
+	@Test
+	public void fieldAndMethodInjection() {
+		Module module = new AbstractModule() {
+			@Override
+			protected void configure() {
+				bind(IMyService.class).to(MyService.class);
+			}
+		};
+		Injector injector = Guice.createInjector(module);
+		MyClientWithInjectedField client1 = injector.getInstance(MyClientWithInjectedField.class);
+		MyClientWithInjectedMethod client2 = injector.getInstance(MyClientWithInjectedMethod.class);
+		assertNotNull(client1.service);
+		assertNotNull(client2.service);
+	}
+
+}
