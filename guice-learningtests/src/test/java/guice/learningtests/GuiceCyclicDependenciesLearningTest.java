a93d596adbe5187a8583f32a8068b25a87a7e1f5
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-02-22 10:44:24 +0100

    cyclicDependencies

---
+++ guice-learningtests/src/test/java/guice/learningtests/GuiceCyclicDependenciesLearningTest.java
@@ -0,0 +1,91 @@
+package guice.learningtests;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
+
+import org.junit.Test;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+import com.google.inject.Module;
+import com.google.inject.Provider;
+import com.google.inject.assistedinject.Assisted;
+import com.google.inject.assistedinject.FactoryModuleBuilder;
+
+public class GuiceCyclicDependenciesLearningTest {
+
+	private static interface IMyView {
+
+	}
+
+	private static class MyView implements IMyView {
+		IMyController controller;
+
+		public void setController(IMyController controller) {
+			this.controller = controller;
+		}
+	}
+
+	private static interface IMyRepository {
+
+	}
+
+	private static class MyRepository implements IMyRepository {
+
+	}
+
+	private static interface IMyController {
+
+	}
+
+	private static class MyController implements IMyController {
+		IMyView view;
+		IMyRepository repository;
+
+		@Inject
+		public MyController(
+			@Assisted IMyView view, // from the instance's creator
+			IMyRepository repository // from the Injector
+		) {
+			this.view = view;
+			this.repository = repository;
+		}
+	}
+
+	private static interface MyControllerFactory {
+		IMyController create(IMyView view);
+	}
+
+	private static class MyViewProvider implements Provider<MyView> {
+		@Inject
+		private MyControllerFactory controllerFactory;
+
+		@Override
+		public MyView get() {
+			// manually solve the cycle
+			MyView view = new MyView();
+			view.setController(controllerFactory.create(view));
+			return view;
+		}
+	}
+
+	@Test
+	public void cyclicDependencies() {
+		Module module = new AbstractModule() {
+			@Override
+			protected void configure() {
+				bind(IMyRepository.class).to(MyRepository.class);
+				bind(MyView.class).toProvider(MyViewProvider.class);
+				install(new FactoryModuleBuilder()
+					.implement(IMyController.class, MyController.class)
+					.build(MyControllerFactory.class));
+			}
+		};
+		Injector injector = Guice.createInjector(module);
+		MyView view = injector.getInstance(MyView.class);
+		assertSame(view, ((MyController) view.controller).view);
+		assertNotNull(((MyController) view.controller).repository);
+	}
+}
